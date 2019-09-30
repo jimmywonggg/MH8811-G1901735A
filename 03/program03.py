99b925d7d0a3f3b93cc4102b20877c8e71d5ae2e
Jimmy <447395322@qq.com>
2019-09-30 12:14:36 +0800

    Assignment3

--- 03/program03.py
+++ 03/program03.py
@@ -1,8 +1,8 @@
 print('welcome!')
-print('input the number you would like to choose. or input "done" to quit this program')
+print('input the number you would like to choose. or input "exit" to quit this program')
 print('01-say hello to the world!\n02- say hello to someone!\n03-know the temp')
 while True:
-    a=input('which one would you like to choose?(01 or 02 or 03 or done)')
+    a=input('which one would you like to choose?(01 or 02 or 03 or exit)')
     if a =="01":
         print('hello,world')
         continue
